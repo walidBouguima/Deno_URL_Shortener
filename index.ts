2c6959bf686d7257c3843bb44bd06fc55aa793cd
Walid Bouguima <walid.bouguima@gmail.com>
2020-10-18 16:41:23 +0100

    Add an array of books as Map data structure, define one item there, define router const, define the endpoints and their repsonse bodies, use the router routes and the allowed methods

--- index.ts
+++ index.ts
@@ -1,25 +1,26 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
-const app = new Application();
+const app = new Application()
+const books = new Map<string, any>(); 
+books.set("1", {
+    id: "1", 
+    title: "The Hound of Baskerviller", 
+    author: "Canon Doyle, Arthur"
+}); 
 
-// Logger
-app.use(async (ctx, next) => {
-  await next();
-  const rt = ctx.response.headers.get("X-Response-Time");
-  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
-});
+const router = new Router(); 
 
-// Timing
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  ctx.response.headers.set("X-Response-Time", `${ms}ms`);
-});
+router
+.get("/", (context) => {
+    context.response.body = "Hello world this is the landing page"
+})
+.get("/book/:id", (context)=> {
+    if(context.params && context.params.id && books.has(context.params.id)){
+        context.response.body = books.get(context.params.id)
+    }
+})
 
-// Hello World!
-app.use((ctx) => {
-  ctx.response.body = "Hello Walid!";
-});
+app.use(router.routes())
+app.use(router.allowedMethods())
 
 await app.listen({ port: 8000 });
