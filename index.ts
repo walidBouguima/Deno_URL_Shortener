73ddd1386c4bdc608c6cc2bafa4b99a73496087a
Walid Bouguima <walid.bouguima@gmail.com>
2020-10-18 17:05:23 +0100

    Add a route to the shortened url and modified the body of the routes as follows: Check if the provided param is attached to the route, check if the urlid is in the paramter list, check if the urlid matches one of th provided urls in JSON file data. If the url param matches all these requirements the we call .dest on the urlid to take th user to the dest address. If not we show him a nice 404

--- index.ts
+++ index.ts
@@ -15,14 +15,18 @@ router
 .get("/", (context) => {
     context.response.body = "Hello world this is the landing page"
 })
-.get("/book/:id", (context)=> {
-    if(context.params && context.params.id && books.has(context.params.id)){
-        context.response.body = books.get(context.params.id)
-    }
-})
-.get("/urls", (context)=> {
+.get("/shrt", (context)=> {
     context.response.body = urls
 })
+
+.get("/shrt/:urlid", (context)=> {
+    if(context.params && context.params.urlid && urls[context.params.urlid]){
+        context.response.redirect(urls[context.params.urlid].dest);
+    }else{
+        context.response.body= "404";
+    }
+})
+
 app.use(router.routes())
 app.use(router.allowedMethods())
 
